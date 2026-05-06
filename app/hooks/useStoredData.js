32b5eec3689c9d764372d6675b40f38341d83ff8
Sujitha-ki <sujitha.sakthivel@timelapseinformatics.com>
2026-05-06 15:37:12 +0530

    useStoredData modified

--- app/hooks/useStoredData.js
+++ app/hooks/useStoredData.js
@@ -1,19 +1,43 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// const useStoredData = () => {
+//   const [userData, setUserData] = useState(null);
+//   const [selectedCompany, setSelectedCompany] = useState(null);
+
+//   useEffect(() => {
+//     const load = async () => {
+//       const user = await AsyncStorage.getItem("userData");
+//       const company = await AsyncStorage.getItem("selectedCompany");
+//       if (user) setUserData(JSON.parse(user));
+//       if (company) setSelectedCompany(JSON.parse(company));
+//     };
+//     load();
+//   }, []);
+
+//   return { userData, selectedCompany };
+// };
+
+// export default useStoredData;
+import { useState, useCallback } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useFocusEffect } from "@react-navigation/native";
 
 const useStoredData = () => {
   const [userData, setUserData] = useState(null);
   const [selectedCompany, setSelectedCompany] = useState(null);
 
-  useEffect(() => {
-    const load = async () => {
-      const user = await AsyncStorage.getItem("userData");
-      const company = await AsyncStorage.getItem("selectedCompany");
-      if (user) setUserData(JSON.parse(user));
-      if (company) setSelectedCompany(JSON.parse(company));
-    };
-    load();
-  }, []);
+  useFocusEffect(
+    useCallback(() => {
+      const load = async () => {
+        const user = await AsyncStorage.getItem("userData");
+        const company = await AsyncStorage.getItem("selectedCompany");
+        if (user) setUserData(JSON.parse(user));
+        if (company) setSelectedCompany(JSON.parse(company));
+      };
+      load();
+    }, []),
+  );
 
   return { userData, selectedCompany };
 };
